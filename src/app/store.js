006d61327d84a8e3616e5db32468eb3437d4b701
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2024-05-03 09:24:32 +0700

    missing image

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pokemonReducers from "../features/pokemons/pokemonSlice";
+
 const store = configureStore({
-    reducer: {
-        pokemons: pokemonReducers
-    }
+  reducer: {
+    pokemons: pokemonReducers,
+  },
 });
 
 export default store;
